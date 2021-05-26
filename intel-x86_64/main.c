64b437a222b7949489458a78916fe571f1d9d622
Konrad Wojda <konrad2001@outlook.com>
2021-05-27 00:49:18 +0200

    Added comments.

--- intel-x86_64/main.c
+++ intel-x86_64/main.c
@@ -7,6 +7,9 @@
 #include <string.h>
 #include <gtk/gtk.h>
 
+/*
+Params for Gtk GUI
+*/
 GtkAdjustment* param_a;
 GtkAdjustment* param_b;
 GtkAdjustment* param_c;
@@ -14,25 +17,30 @@ GtkAdjustment* param_s;
 GtkImage* gtk_bitmap;
 
 
-
+/*
+Constants used for operate on BMP files
+*/
 #define OUTPUT_FILE_NAME "output.bmp"
 
-#define BMP_HEADER_SIZE 54
+#define HEADER_SIZE 54
 
-#define BMP_PIXEL_OFFSET 54
+#define PIXEL_OFFSET 54
 
-#define BMP_PLANES 1
+#define PLANES 1
 
-#define BMP_BPP_TYPE 24
+#define BPP_TYPE 24
 
-#define BMP_HORIZONTAL_RES 500
+#define HORIZONTAL_RESOLUTION 500
 
-#define BMP_VERTICAL_RES 500
+#define VERTICAL_RESOLUTION 500
 
-#define BMP_DIB_HEADER_SIZE 40
+#define DIB_HEADER_SIZE 40
 
 void draw_bitmap(unsigned char* bitmap, double a, double b, double c, double s);
 
+/*
+Struct to represent basic BMP Header
+*/
 typedef struct {
     unsigned char sig_b;
     unsigned char sig_m;
@@ -46,30 +54,36 @@ typedef struct {
     uint16_t bpp_type;
     uint32_t compression;
     uint32_t image_size;
-    uint32_t horizontal_res;
+    uint32_t horizonal_res;
     uint32_t vertical_res;
     uint32_t color_plette;
     uint32_t important_colors;
 } BmpHeader;
 
-void init_bmp_header(BmpHeader *header)
+/*
+Initializes BMP Header with default values
+*/
+void initialize_bmp_header(BmpHeader *header)
 {
     header->sig_b = 'B';
     header->sig_m = 'M';
     header->reserved = 0;
-    header->pixel_offset = BMP_PIXEL_OFFSET;
-    header->header_size = BMP_DIB_HEADER_SIZE;
-    header->planes = BMP_PLANES;
-    header->bpp_type = BMP_BPP_TYPE;
+    header->pixel_offset = PIXEL_OFFSET;
+    header->header_size = DIB_HEADER_SIZE;
+    header->planes = PLANES;
+    header->bpp_type = BPP_TYPE;
     header->compression = 0;
     header->image_size = 0;
-    header->horizontal_res = BMP_HORIZONTAL_RES;
-    header->vertical_res = BMP_VERTICAL_RES;
+    header->horizonal_res = HORIZONTAL_RESOLUTION;
+    header->vertical_res = VERTICAL_RESOLUTION;
     header->color_plette = 0;
     header->important_colors = 0;
 }
 
-void write_bytes_to_bmp(unsigned char *buffer, size_t size)
+/*
+Writes given buffer to BMP file
+*/
+void write_buffer_to_bmp(unsigned char *buffer, size_t size)
 {
     FILE *file;
 
@@ -84,47 +98,75 @@ void write_bytes_to_bmp(unsigned char *buffer, size_t size)
 }
 
 
-
+/*
+Generates empty bitmap of given width and height and fills it with white color.
+*/
 unsigned char *generate_white_bitmap(unsigned int width, unsigned int height, size_t *output_size)
 {
+    //Calculate row size
     unsigned int row_size = (width * 3 + 3) & ~3;
-    *output_size = row_size * height + BMP_HEADER_SIZE;
+
+    //Calculate output size
+    *output_size = row_size * height + HEADER_SIZE;
+
+    //Allocs memory for bitmap
     unsigned char *bitmap = (unsigned char *) malloc(*output_size);
 
+    //Creates and initializes header
     BmpHeader header;
-    init_bmp_header(&header);
+    initialize_bmp_header(&header);
     header.size = *output_size;
     header.width = width;
     header.height = height;
 
-    memcpy(bitmap, &header, BMP_HEADER_SIZE);
-    int i = BMP_HEADER_SIZE;
-    for (BMP_HEADER_SIZE; i < *output_size; ++i)
+    //Copies bytes from header to bitmap
+    memcpy(bitmap, &header, HEADER_SIZE);
+
+    //Fills bitmap with white color
+    for (int i = HEADER_SIZE; i < *output_size; ++i)
     {
         bitmap[i] = 0xff;
     }
     return bitmap;
 }
 
+/*
+Exit function for GUI
+*/
 void exit_exit()
 {
     gtk_main_quit();
 }
 
-
+/*
+Function which is called on GUI param change
+*/
 void draw()
 {
+    //Get the params
     double A = gtk_adjustment_get_value(param_a);
     double B = gtk_adjustment_get_value(param_b);
     double C = gtk_adjustment_get_value(param_c);
     double S = gtk_adjustment_get_value(param_s);
+
+    //Create an empty bitmap
     size_t bmp_size = 0;
     unsigned char *bmp_buffer = generate_white_bitmap(512, 512, &bmp_size);
-    printf("%d\n", sizeof(BmpHeader));
-    printf("%d\n", bmp_size);
+
+    //Debug prints
+    // printf("%d\n", sizeof(BmpHeader));
+    // printf("%d\n", bmp_size);
+
+    //Call assembly function to draw a parabola of given params
     draw_bitmap(bmp_buffer, A, B, C, S);
-    write_bytes_to_bmp(bmp_buffer, bmp_size);
+
+    //Write buffer to file
+    write_buffer_to_bmp(bmp_buffer, bmp_size);
+
+    //Dealloc memory
     free(bmp_buffer);
+
+    //View an image in GUI
     gtk_image_set_from_file(gtk_bitmap, OUTPUT_FILE_NAME);
 }
 
